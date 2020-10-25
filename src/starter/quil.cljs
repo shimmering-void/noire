2783bc6687fbb9d3540d1a67f5f0285f735e145e
Jona Ekenberg <saikyun@gmail.com>
2020-10-25 15:00:14 +0100

    color only affects circle now

--- src/starter/quil.cljs
+++ src/starter/quil.cljs
@@ -21,7 +21,7 @@
 
 (defn draw-state [state]
   ;; Clear the sketch by filling it with light-grey color.
-  (q/background (:color state))
+  (q/background 0)
   ;; Set circle color.
   (q/fill (:color state) 255 255)
   ;; Calculate x and y coordinates of the circle.
